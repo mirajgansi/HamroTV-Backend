0cd02b3197bd802a666793c92ea1823e992ebbd1
mirajgansi <mirajgansi122@gmail.com>
2025-01-20 10:55:30 +0545

    index js error fixed

--- index.js
+++ index.js
@@ -1,22 +1,17 @@
-const express=require('express');
-const cors=require('cors');
-const bodyParser=require('body-parser');
+const {DataType, DataTypes, Model} = require('sequelize');
+const sequelize = require('../database/db');
 
-const app=express();
-
-const PORT=process.env.PORT || 4000;
-
-app.use(cors())
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended:true}))
-
-app.get('/',(req,res)=>{
-    res.send("WELCOME TO THE PAGE")
-})
-
-app.get('/notice',(req,res)=>{
-    res.send("Thi is notice")
-})
-app.listen(PORT,()=>{
-    console.log(`server Running on ...................PORT ${PORT}`)
-})
+const Test= sequelize.define('Test',{
+    id: {type:DataTypes.INTEGER,
+        primaryKey:true,
+        autoIncrement:true,}
+        ,name:{
+            type:DataTypes.STRING(255),
+            allowNull:false,},
+            email:{
+                type:  DataTypes.STRING,
+                allowNull:false,
+                unique:true,
+        },
+});
+module.exports=Test;
